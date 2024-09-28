094c8fd8d331f7103e2a99811018b51bfafd8017
jonzo99 <jonzojimenez@gmail.com>
2024-09-28 11:56:44 -0700

    added xcode project

---
+++ SoccerInstitute/SoccerInstituteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerInstituteApp.swift
+//  SoccerInstitute
+//
+//  Created by Jonzo Jimenez on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SoccerInstituteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoccerInstituteTests/SoccerInstituteTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerInstituteTests.swift
+//  SoccerInstituteTests
+//
+//  Created by Jonzo Jimenez on 9/28/24.
+//
+
+import Testing
+@testable import SoccerInstitute
+
+struct SoccerInstituteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
